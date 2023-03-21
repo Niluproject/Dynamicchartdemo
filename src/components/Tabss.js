ea13025cead487c380b4201ffc242bc76605e4d7
nilesh.v <nilesh.v@covrize.com>
2023-03-21 11:58:00 +0530

    changes

--- src/components/Tabss.js
+++ src/components/Tabss.js
@@ -1,8 +1,21 @@
+import '../App.css';
 import React from 'react'
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
 
 function Tabss() {
+    const investor = {
+        name: 'Investors grid',
+        imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+        imageSize: 90,
+    };
+
+    const startups = {
+        name: 'StartUps grid',
+        imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+        imageSize: 90,
+    };
+
     return (
         <div>
             <Tabs>
@@ -12,10 +25,28 @@ function Tabss() {
                 </TabList>
 
                 <TabPanel>
-                    <h2>Investors grid</h2>
+                    <h2>{investor.name}</h2>
+                    <img
+                        className="avatar"
+                        src={investor.imageUrl}
+                        alt={'Photo of ' + investor.name}
+                        style={{
+                            width: investor.imageSize,
+                            height: investor.imageSize
+                        }}
+                    />
                 </TabPanel>
                 <TabPanel>
-                    <h2>StartUps grid</h2>
+                    <h2>{startups.name}</h2>
+                    <img
+                        className="avatar"
+                        src={startups.imageUrl}
+                        alt={'Photo of ' + startups.name}
+                        style={{
+                            width: startups.imageSize,
+                            height: startups.imageSize
+                        }}
+                    />
                 </TabPanel>
             </Tabs>
         </div>
